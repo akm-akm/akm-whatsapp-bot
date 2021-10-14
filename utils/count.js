cfd639f6ab51a774c60e0bed95acc226807ce3e6
ADITYA KUMAR MANDAL <67242659+akm-akm@users.noreply.github.com>
2021-10-14 16:47:49 +0530

    Update count.js

--- utils/count.js
+++ utils/count.js
@@ -4,7 +4,7 @@ const count = (infor, credit = 1) => new Promise(async (resolve, reject) => {
   const number = infor.number;
   const no_of_msg_today = infor.noofmsgtoday + (1 * credit);
   const totalmsg = infor.totalmsg + 1;
-  const no_of_msg_today_group = infor.groupdata.totalmsgtoday;
+  const no_of_msg_today_group = infor.groupdata.totalmsgtoday+1;
   const no_of_msg_group = infor.groupdata.totalmsg + 1;
   const no_of_msg_today_by_bot = infor.botdata.totalmsgtoday + 1
   const no_of_msg_by_bot = infor.botdata.totalmsg + 1
