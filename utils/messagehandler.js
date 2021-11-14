e75b864082fab57f73e5af973a4c5778577262ba
akm-akm <adityanick08@gmail.com>
2021-11-14 20:45:42 +0530

    ok

--- utils/messagehandler.js
+++ utils/messagehandler.js
@@ -1,168 +1,222 @@
 const path = require("path");
-const fs = require('fs');
+const fs = require("fs");
 const { count } = require(path.join(__dirname, "./count"));
 
-const chalk = require('chalk');
+const chalk = require("chalk");
 const sql = require(path.join(__dirname, "./ps"));
 const commandHandler = new Map();
-const plugins = fs.readdirSync(path.join(__dirname, '../plugin'))
+const plugins = fs.readdirSync(path.join(__dirname, "../plugin"));
 for (let file of plugins) {
-
-
-    const command = require(path.join(__dirname, '../plugin/', `${file}`));
-    if (command.name && command.usage && command.desc && typeof command.handle === "function" && command.eg && typeof command.group === "boolean" && typeof command.owner === "boolean") {
-        commandHandler.set(command.name, command);
-    }
+  const command = require(path.join(__dirname, "../plugin/", `${file}`));
+  if (
+    command.name &&
+    command.usage &&
+    command.desc &&
+    typeof command.handle === "function" &&
+    command.eg &&
+    typeof command.group === "boolean" &&
+    typeof command.owner === "boolean"
+  ) {
+    commandHandler.set(command.name, command);
+  }
 }
-const builtInPlugins = fs.readdirSync(path.join(__dirname, '../builtInPlugins'))
+const builtInPlugins = fs.readdirSync(
+  path.join(__dirname, "../builtInPlugins")
+);
 for (let file of builtInPlugins) {
-    const command = require(path.join(__dirname, '../builtInPlugins/', `${file}`));
-    if (command.name && command.usage && command.desc && typeof command.handle === "function" && command.eg && typeof command.group === "boolean" && typeof command.owner === "boolean") {
-        commandHandler.set(command.name, command);
-    }
-
+  const command = require(path.join(
+    __dirname,
+    "../builtInPlugins/",
+    `${file}`
+  ));
+  if (
+    command.name &&
+    command.usage &&
+    command.desc &&
+    typeof command.handle === "function" &&
+    command.eg &&
+    typeof command.group === "boolean" &&
+    typeof command.owner === "boolean"
+  ) {
+    commandHandler.set(command.name, command);
+  }
 }
 
-
-
-
-//sdbsfxfndfftjftjtj
 
 exports.messagehandler = async (Infor) => {
+    /**
+     * @param {String} place
+     * This stores the place message is received
+     */
+  const place = Infor.isGroup ? Infor.groupName : "inbox";
 
-    const place = Infor.isGroup ? Infor.groupName : "inbox";
-
-
-    /* This line checks the following- 
+  /* This line checks the following- 
     (If the message is sent in a group, is the group limit exhausted ?) return
     (Is the message sent in the group more than a word?) continue
     (Is the message sent in a group a media and the autosticker feature turned on?) continue
     */
-    if (!(!Infor.isGroup || (Infor.isGroup && (Infor.groupdata.totalmsgtoday <= Infor.botdata.dailygrouplimit)) &&
-        (Infor.arg.length !== 0 || (Infor.isGroup && Infor.isMedia && Infor.groupdata.autosticker)))
-    ) return
+  if (
+    !(
+      !Infor.isGroup ||
+      (Infor.isGroup &&
+        Infor.groupdata.totalmsgtoday <= Infor.botdata.dailygrouplimit &&
+        (Infor.arg.length !== 0 ||
+          (Infor.isGroup && Infor.isMedia && Infor.groupdata.autosticker)))
+    )
+  )
+    return;
 
-
-
-    /* This line checks the following-
+  /* This line checks the following-
     (is the message sent in a group?) and
     (is the user sending the message banned in that group to use the bot?)
     If yes then return
     */
-    if (Infor.isGroup && Infor.groupdata.banned_users.includes(Infor.number)) return
+  if (Infor.isGroup && Infor.groupdata.banned_users.includes(Infor.number))
+    return;
 
-
-
-    /* This line checks the following-
+  /* This line checks the following-
     (is the message sent in a group?) and
     (Can non admins use the bot in the group?)
     (If non admins cannot use the bot then is the sender an admin or a bot moderator?)
     If yes then continue
     */
-    if (Infor.isGroup && Infor.groupdata.membercanusebot === false && !Infor.isGroupAdmins) return
+  if (
+    Infor.isGroup &&
+    Infor.groupdata.membercanusebot === false &&
+    !Infor.isGroupAdmins
+  )
+    return;
 
-
-
-    /* This line checks the following-
+  /* This line checks the following-
     (is the message sent 'limit' ?)
     If yes then send back the credits used
     */
-    if (Infor.arg[0] === "limit") {
-        const x =
-            Infor.mess.limit + Infor.noofmsgtoday + " / *" + Infor.botdata.dailylimit + "*";
-        Infor.replytext(x)
-        return;
-    }
+  if (Infor.arg[0] === "limit") {
+    const x =
+      Infor.mess.limit +
+      Infor.noofmsgtoday +
+      " / *" +
+      Infor.botdata.dailylimit +
+      "*";
+    Infor.replytext(x);
+    return;
+  }
 
-
-
-    /* This line checks the following-
+  /* This line checks the following-
     (IS the number of message sent by the bot less than the daily limit) and
     (the sender is not bot moderator) and
     (the sender is not the owner) and
     (Daily limity is not over)
     If yes then continue
     */
-    if (
-        Infor.noofmsgtoday >= Infor.botdata.dailylimit &&
-        !Infor.botdata.moderators.includes(Infor.number) &&
-        Infor.dailylimitover === false
-    ) {
-        sql.query(`UPDATE messagecount SET dailylimitover = true WHERE phonenumber ='${Infor.number}';`)
-        Infor.replytext(Infor.mess.userlimit)
-    }
+  if (
+    Infor.noofmsgtoday >= Infor.botdata.dailylimit &&
+    !Infor.botdata.moderators.includes(Infor.number) &&
+    Infor.dailylimitover === false
+  ) {
+    sql.query(
+      `UPDATE messagecount SET dailylimitover = true WHERE phonenumber ='${Infor.number}';`
+    );
+    Infor.replytext(Infor.mess.userlimit);
+  }
 
-
-
-    /* This line checks the following-
+  /* This line checks the following-
     (IS the daily limit over?)
     If yes then returntrtuj
     */
-    if (Infor.dailylimitover === true) return
+  if (Infor.dailylimitover === true) return;
 
+  if (
+    Infor.isGroup &&
+    Infor.groupdata.totalmsgtoday >= Infor.botdata.dailygrouplimit
+  ) {
+    Infor.text(Infor.mess.grouplimit);
+    count(Infor);
+    return;
+  }
 
+  console.log(
+    "🤖  " +
+      chalk.bgRed("[" + Infor.number + "   ]") +
+      "  " +
+      chalk.bgGreen("[" + place + "]") +
+      "  " +
+      chalk.bgBlue("[" + Infor.arg.slice(0, 6).join(" ") + "]")
+  );
 
+  if (Infor.abusepresent.length != 0 && !Infor.isBotModerator) {
+    Infor.replytext("⚠️  ```" + Infor.abusepresent.join(" ") + "```");
+    count(Infor);
+    return;
+  }
 
-    if (Infor.isGroup && Infor.groupdata.totalmsgtoday >= Infor.botdata.dailygrouplimit) {
-        Infor.text(Infor.mess.grouplimit);
-        count(Infor);
-        return
+  if (
+    Infor.isGroup &&
+    Infor.groupdata.autosticker &&
+    Infor.isMedia &&
+    Infor.arg[0] !== "sticker" &&
+    Infor.arg[0] !== "testnsfw"
+  ) {
+    commandHandler.get("sticker").handle(Infor);
+    count(Infor);
+    return;
+  }
+
+  if (
+    Infor.arg == "hi" ||
+    Infor.arg == "hey" ||
+    Infor.arg == "hello" ||
+    Infor.arg == "helloo" ||
+    Infor.arg == "hellooo" ||
+    Infor.arg == "hii" ||
+    Infor.arg == "hiii" ||
+    Infor.arg == "heyy" ||
+    Infor.arg == "heyyy"
+  ) {
+    Infor.replytext(
+      Infor.mess.salutations[
+        Math.floor(Math.random() * Infor.mess.salutations.length)
+      ]
+    );
+
+    count(Infor);
+    return;
+  }
+
+  if (commandHandler.has(Infor.arg[0])) {
+    if (commandHandler.get(Infor.arg[0]).owner && !Infor.isOwner) {
+      Infor.replytext(Infor.mess.only.ownerB);
+      count(Infor);
+      return;
+    } else if (commandHandler.get(Infor.arg[0]).owner && Infor.isOwner) {
+      commandHandler.get(Infor.arg[0]).handle(Infor);
+      count(Infor);
+      return;
+    } else if (commandHandler.get(Infor.arg[0]).group && !Infor.isGroup) {
+      Infor.replytext(Infor.mess.only.group);
+      count(Infor);
+      return;
+    } else if (
+      commandHandler.get(Infor.arg[0]).group &&
+      Infor.isGroup &&
+      !Infor.isGroupAdmins
+    ) {
+      Infor.replytext(Infor.mess.only.admin);
+      count(Infor);
+    } else if (
+      commandHandler.get(Infor.arg[0]).group &&
+      Infor.isGroup &&
+      Infor.isGroupAdmins
+    ) {
+      commandHandler.get(Infor.arg[0]).handle(Infor);
+      count(Infor);
+    } else {
+      commandHandler.get(Infor.arg[0]).handle(Infor);
+      count(Infor);
+      return;
     }
-
-    console.log("🤖  " + chalk.bgRed("[" + Infor.number + ']') + "  " +
-        chalk.bgGreen("[" + place + ']')
-        + "  " + chalk.bgBlue("[" + Infor.arg.slice(0, 6).join(" ") + ']'));
-
-    if (Infor.abusepresent.length != 0 && !Infor.isBotModerator) {
-        Infor.replytext("⚠️  ```" + Infor.abusepresent.join(" ") + "```")
-        count(Infor)
-        return;
-    }
-
-    if (Infor.isGroup && Infor.groupdata.autosticker && Infor.isMedia && Infor.arg[0] !== "sticker" && Infor.arg[0] !== "testnsfw") {
-        commandHandler.get('sticker').handle(Infor);
-        count(Infor)
-        return;
-    }
-
-
-    if (Infor.arg == "hi" || Infor.arg == "hey" || Infor.arg == "hello" || Infor.arg == "helloo" || Infor.arg == "hellooo" || Infor.arg == "hii" || Infor.arg == "hiii" || Infor.arg == "heyy" || Infor.arg == "heyyy" ) {
-        Infor.replytext(Infor.mess.salutations[Math.floor(Math.random() * Infor.mess.salutations.length)])
-        count(Infor);
-        return;
-    }
-
-
-
-    if (commandHandler.has(Infor.arg[0])) {
-
-        if (commandHandler.get(Infor.arg[0]).owner && !Infor.isOwner) {
-            Infor.replytext(Infor.mess.only.ownerB);
-            count(Infor);
-            return
-        } else if (commandHandler.get(Infor.arg[0]).owner && Infor.isOwner) {
-            commandHandler.get(Infor.arg[0]).handle(Infor);
-            count(Infor);
-            return
-        } else if (commandHandler.get(Infor.arg[0]).group && (!Infor.isGroup)) {
-            Infor.replytext(Infor.mess.only.group);
-            count(Infor);
-            return;
-        } else if (commandHandler.get(Infor.arg[0]).group && Infor.isGroup && !Infor.isGroupAdmins) {
-            Infor.replytext(Infor.mess.only.admin);
-            count(Infor);
-        } else if (commandHandler.get(Infor.arg[0]).group && Infor.isGroup && Infor.isGroupAdmins) {
-            commandHandler.get(Infor.arg[0]).handle(Infor);
-            count(Infor);
-
-        } else {
-            commandHandler.get(Infor.arg[0]).handle(Infor);
-            count(Infor);
-            return;
-        }
-    } else if ((Infor.isGroup && Infor.groupdata.useprefix) || !Infor.isGroup) {
-        Infor.replytext(Infor.mess.unknowncommand);
-        count(Infor);
-    }
-
-}
+  } else if ((Infor.isGroup && Infor.groupdata.useprefix) || !Infor.isGroup) {
+    Infor.replytext(Infor.mess.unknowncommand);
+    count(Infor);
+  }
+};
--- utils/settingcheck.js
+++ utils/settingcheck.js
@@ -1,35 +1,36 @@
 const path = require("path");
 const fs = require("fs");
-const chalk = require('chalk');
+const chalk = require("chalk");
 const sql = require(path.join(__dirname, "./ps"));
 const settings = JSON.parse(
   fs.readFileSync(path.join(__dirname, "../data/settings.json"))
 );
-const {
-  newgroup
-} = require(path.join(__dirname, "./newgroup"));
+const { newgroup } = require(path.join(__dirname, "./newgroup"));
 
-const InforClass = require('./Infor');
+const InforClass = require("./Infor");
 
-
-let data1, data3 = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "../data/data3.json")));
+let data1,
+  data3 = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "../data/data3.json"))
+  );
 const urlregex =
   /^(?:(?:https?|http|www):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
 Array.prototype.detecta = function () {
   const returnarray = [];
   this.forEach((element, index) => {
-    let hash = 0, i, chr;
+    let hash = 0,
+      i,
+      chr;
     if (element.length === 0) return hash;
     for (i = 0; i < element.length; i++) {
       chr = element.charCodeAt(i);
-      hash = ((hash << 5) - hash) + chr;
+      hash = (hash << 5) - hash + chr;
       hash |= 0;
     }
     data3.words.indexOf(hash) != -1 ? returnarray.push(this[index]) : null;
-  })
+  });
   return returnarray;
-}
+};
 const getGroupAdmins = (participants) => {
   admins = [];
   for (let i of participants) {
@@ -39,125 +40,197 @@ const getGroupAdmins = (participants) => {
 };
 
 module.exports = async function settingread(xxx, client) {
-
   const Infor = new InforClass();
   Infor.client = client;
   Infor.reply = xxx;
   try {
-
     const random = settings.prefixchoice.charAt(
-      Math.floor(Math.random() * settings.prefixchoice.length))
-
+      Math.floor(Math.random() * settings.prefixchoice.length)
+    );
 
     const from = xxx.key.remoteJid;
     Infor.from = from;
     Infor.isGroup = from.endsWith("@g.us");
     Infor.sender = Infor.isGroup ? xxx.participant : xxx.key.remoteJid;
-    Infor.groupMetadata = Infor.isGroup ? await client.groupMetadata(from) : undefined;
-    Infor.groupMembers = Infor.isGroup ? Infor.groupMetadata.participants : undefined;
-    Infor.groupAdmins = Infor.isGroup ? getGroupAdmins(Infor.groupMembers) : undefined;
+    Infor.groupMetadata = Infor.isGroup
+      ? await client.groupMetadata(from)
+      : undefined;
+    Infor.groupMembers = Infor.isGroup
+      ? Infor.groupMetadata.participants
+      : undefined;
+    Infor.groupAdmins = Infor.isGroup
+      ? getGroupAdmins(Infor.groupMembers)
+      : undefined;
     Infor.groupName = Infor.isGroup ? Infor.groupMetadata.subject : undefined;
     Infor.botNumber = client.user.jid.split("@")[0];
-    Infor.isBotGroupAdmins = Infor.isGroup ? Infor.groupAdmins.includes(`${Infor.botNumber}@s.whatsapp.net`) || false : undefined;
-    Infor.isOwner = Infor.isGroup ?
-      Infor.sender.split("@")[0] === process.env.OWNER_NUMBER : Infor.from.split("@")[0] === process.env.OWNER_NUMBER;
-    Infor.isSuperAdmin = Infor.isGroup ? Infor.groupMetadata.owner == Infor.from : undefined;
+    Infor.isBotGroupAdmins = Infor.isGroup
+      ? Infor.groupAdmins.includes(`${Infor.botNumber}@s.whatsapp.net`) || false
+      : undefined;
+    Infor.isOwner = Infor.isGroup
+      ? Infor.sender.split("@")[0] === process.env.OWNER_NUMBER
+      : Infor.from.split("@")[0] === process.env.OWNER_NUMBER;
+    Infor.isSuperAdmin = Infor.isGroup
+      ? Infor.groupMetadata.owner == Infor.from
+      : undefined;
 
-
-    const botdata = await sql.query(
-      "select * from botdata;"
-    );
+    const botdata = await sql.query("select * from botdata;");
 
     if (Infor.isGroup) {
-
-      data1 = await sql.query(`select * from groupdata where groupid='${from}';`);
+      data1 = await sql.query(
+        `select * from groupdata where groupid='${from}';`
+      );
       if (data1.rows.length == 0) {
-
-        if (process.env.NODE_ENV === 'development') {
-          console.log("👪  " + chalk.bgCyan("Prefix assigned is / for group " + Infor.groupName));
+        if (process.env.NODE_ENV === "development") {
+          console.log(
+            "👪  " +
+              chalk.bgCyan("Prefix assigned is / for group " + Infor.groupName)
+          );
           await sql.query(
             `INSERT INTO groupdata VALUES ('${from}','true','/','false','true', '{''}',0,0,false,true);`
           );
-          return settingread(xxx, client)
+          return settingread(xxx, client);
         }
-        if (process.env.NODE_ENV === 'production') {
+        if (process.env.NODE_ENV === "production") {
           if (
-            Infor.groupMetadata.participants.length < botdata.rows[0].mingroupsize
+            Infor.groupMetadata.participants.length <
+            botdata.rows[0].mingroupsize
           ) {
-            Infor.text("*Minimum participants required is* " + botdata.rows[0].mingroupsize);
+            Infor.text(
+              "*Minimum participants required is* " +
+                botdata.rows[0].mingroupsize
+            );
             client.groupLeave(from);
-            return
+            return;
           }
-          console.log("👪  " + chalk.bgCyan(`Prefix assigned is '${random}' for group ` + Infor.groupName));
-          newgroup(from, client, random)
+          console.log(
+            "👪  " +
+              chalk.bgCyan(
+                `Prefix assigned is '${random}' for group ` + Infor.groupName
+              )
+          );
+          newgroup(from, client, random);
           sql.query(
             `INSERT INTO groupdata VALUES ('${from}','true','${random}','false','true', '{''}',0,0,false,true);`
           );
-          return settingread(xxx, client)
+          return settingread(xxx, client);
         }
       }
     }
 
-
-    const number = Infor.isGroup ? Infor.sender.split("@")[0] : from.split("@")[0];
+    const number = Infor.isGroup
+      ? Infor.sender.split("@")[0]
+      : from.split("@")[0];
     const data2 = await sql.query(
-      `select * from messagecount where phonenumber='${number}';`)
+      `select * from messagecount where phonenumber='${number}';`
+    );
     if (data2.rows.length == 0) {
-      console.log("👨  " + chalk.bgBlueBright("Entering data for  number -" + number));
-      await sql.query(`INSERT INTO messagecount VALUES ('${number}', 0, 0, false);`)
-      return settingread(xxx, client)
+      console.log(
+        "👨  " + chalk.bgBlueBright("Entering data for  number -" + number)
+      );
+      await sql.query(
+        `INSERT INTO messagecount VALUES ('${number}', 0, 0, false);`
+      );
+      return settingread(xxx, client);
     }
-
-
 
     const type = Object.keys(xxx.message)[0];
     const content = JSON.stringify(xxx.message);
     try {
       stanzaId =
-        type == "extendedTextMessage" ?
-          xxx.message.extendedTextMessage.contextInfo
-            .stanzaId || undefined :
-          0;
+        type == "extendedTextMessage"
+          ? xxx.message.extendedTextMessage.contextInfo.stanzaId || undefined
+          : 0;
     } catch (error) {
       stanzaId = 0;
     }
 
     let arg =
-      type === "conversation" ?
-        xxx.message.conversation :
-        type === "imageMessage" ?
-          xxx.message.imageMessage.caption :
-          type === "videoMessage" ?
-            xxx.message.videoMessage.caption :
-            type == "extendedTextMessage" ?
-              xxx.message.extendedTextMessage.text :
-              "";
+      type === "conversation"
+        ? xxx.message.conversation
+        : type === "imageMessage"
+        ? xxx.message.imageMessage.caption
+        : type === "videoMessage"
+        ? xxx.message.videoMessage.caption
+        : type == "extendedTextMessage"
+        ? xxx.message.extendedTextMessage.text
+        : "";
 
     Infor.isMedia = type === "imageMessage" || type === "videoMessage";
-    Infor.arg = Infor.isGroup ? data1.rows[0].useprefix ? arg.replace(/\s+/g, " ").toLowerCase().startsWith(data1.rows[0].prefix) ?
-      arg = (arg.replace(/\s+/g, " ").replace(/^\s+|\s+$/g, "").slice(1).replace(/^\s+|\s+$/g, "").split(" ")).map(xa => urlregex.test(xa) ? xa : xa.toLowerCase()) :
-      arg = [] : arg = (arg.replace(/\s+/g, " ").replace(/^\s+|\s+$/g, "").split(" ")).map(xa =>
-        urlregex.test(xa) ? xa : xa.toLowerCase()) : arg.replace(/\s+/g, " ").startsWith('!') || arg.replace(/\s+/g, " ").startsWith('.') || arg.replace(/\s+/g, " ").startsWith('#') || arg.replace(/\s+/g, " ").startsWith('-') ? arg = (arg.slice(1).replace(/^\s+|\s+$/g, "").replace(/\s+/g, " ").split(" ")).map(xa => urlregex.test(xa) ? xa : xa.toLowerCase()) : arg = (arg.replace(/\s+/g, " ").split(" ")).map(xa => urlregex.test(xa) ? xa : xa.toLowerCase());
+    Infor.arg = Infor.isGroup
+      ? data1.rows[0].useprefix
+        ? arg
+            .replace(/\s+/g, " ")
+            .toLowerCase()
+            .startsWith(data1.rows[0].prefix)
+          ? (arg = arg
+              .replace(/\s+/g, " ")
+              .replace(/^\s+|\s+$/g, "")
+              .slice(1)
+              .replace(/^\s+|\s+$/g, "")
+              .split(" ")
+              .map((xa) => (urlregex.test(xa) ? xa : xa.toLowerCase())))
+          : (arg = [])
+        : (arg = arg
+            .replace(/\s+/g, " ")
+            .replace(/^\s+|\s+$/g, "")
+            .split(" ")
+            .map((xa) => (urlregex.test(xa) ? xa : xa.toLowerCase())))
+      : arg.replace(/\s+/g, " ").startsWith("!") ||
+        arg.replace(/\s+/g, " ").startsWith(".") ||
+        arg.replace(/\s+/g, " ").startsWith("#") ||
+        arg.replace(/\s+/g, " ").startsWith("-")
+      ? (arg = arg
+          .slice(1)
+          .replace(/^\s+|\s+$/g, "")
+          .replace(/\s+/g, " ")
+          .split(" ")
+          .map((xa) => (urlregex.test(xa) ? xa : xa.toLowerCase())))
+      : (arg = arg
+          .replace(/\s+/g, " ")
+          .split(" ")
+          .map((xa) => (urlregex.test(xa) ? xa : xa.toLowerCase())));
     Infor.number = number;
     Infor.noofmsgtoday = data2.rows[0].totalmsgtoday;
     Infor.totalmsg = data2.rows[0].totalmsg;
     Infor.dailylimitover = data2.rows[0].dailylimitover;
-    Infor.abusepresent = from.endsWith("@g.us") ? data1.rows[0].allowabuse == 0 ? arg.detecta() : [] : arg.detecta();
+    Infor.abusepresent = from.endsWith("@g.us")
+      ? data1.rows[0].allowabuse == 0
+        ? arg.detecta()
+        : []
+      : arg.detecta();
     Infor.groupdata = from.endsWith("@g.us") ? data1.rows[0] : 0;
     Infor.botdata = botdata.rows[0];
     Infor.stanzaId = stanzaId;
-    Infor.isQuotedImage = type === "extendedTextMessage" && content.includes("imageMessage");
-    Infor.isQuotedVideo = type === "extendedTextMessage" && content.includes("videoMessage");
-    Infor.isQuotedText = type == "extendedTextMessage" && content.includes("text") && content.includes("stanzaId");
-    Infor.isQuotedSticker = type === 'extendedTextMessage' && content.includes('stickerMessage')
-    Infor.quotedMessage = Infor.isQuotedText ? Infor.reply.message.extendedTextMessage.contextInfo.quotedMessage.conversation : undefined;
-    Infor.isUserTagged = type == "extendedTextMessage" && content.includes("text") && content.includes("mentionedJid");
-    Infor.taggedUser = Infor.isUserTagged ? xxx.message.extendedTextMessage.contextInfo.mentionedJid : undefined;
-    Infor.isBotModerator = Infor.botdata.moderators.includes(Infor.number) || Infor.isOwner;
-    Infor.isGroupAdmins = Infor.isGroup ? Infor.groupAdmins.includes(Infor.sender) || Infor.isBotModerator || false : undefined;
+    Infor.isQuotedImage =
+      type === "extendedTextMessage" && content.includes("imageMessage");
+    Infor.isQuotedVideo =
+      type === "extendedTextMessage" && content.includes("videoMessage");
+    Infor.isQuotedText =
+      type == "extendedTextMessage" &&
+      content.includes("text") &&
+      content.includes("stanzaId");
+    Infor.isQuotedSticker =
+      type === "extendedTextMessage" && content.includes("stickerMessage");
+    Infor.quotedMessage = Infor.isQuotedText
+      ? Infor.reply.message.extendedTextMessage.contextInfo.quotedMessage
+          .conversation
+      : undefined;
+    Infor.isUserTagged =
+      type == "extendedTextMessage" &&
+      content.includes("text") &&
+      content.includes("mentionedJid");
+    Infor.taggedUser = Infor.isUserTagged
+      ? xxx.message.extendedTextMessage.contextInfo.mentionedJid
+      : undefined;
+    Infor.isBotModerator =
+      Infor.botdata.moderators.includes(Infor.number) || Infor.isOwner;
+    Infor.isGroupAdmins = Infor.isGroup
+      ? Infor.groupAdmins.includes(Infor.sender) ||
+        Infor.isBotModerator ||
+        false
+      : undefined;
 
     return Infor;
-
   } catch (error) {
     console.log(error);
     // Infor.errorlog(error);
