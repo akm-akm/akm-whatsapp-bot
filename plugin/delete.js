e494abfbee0942e1b735150374a591047fb49647
akm-akm <adityanick08@gmail.com>
2022-08-06 17:49:45 +0530

    ok

--- plugin/delete.js
+++ plugin/delete.js
@@ -8,18 +8,19 @@ module.exports = {
   async handle(Bot) {
     try {
       if (!Bot.stanzaId) {
-        Bot.wrongCommand();
-        return;
+        return Bot.wrongCommand();
       }
       const options = {
-        remoteJid: Bot.botNumber + "@s.whatsapp.net",
+        // remoteJid: Bot.from,
         fromMe: true,
-        id: Bot.reply.message.extendedTextMessage.contextInfo.stanzaId
+        id: Bot.stanzaId
       };
+      //   console.log(Bot.reply.message.extendedTextMessage.contextInfo);
       await Bot.client.sendMessage(Bot.from, {
         delete: options
       });
     } catch (error) {
+      // Bot.replytext(Bot.mess.only.Badmin);
       Bot.replytext(Bot.mess.error.error);
     }
   }
